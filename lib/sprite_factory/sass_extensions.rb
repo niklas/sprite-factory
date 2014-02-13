5d52d444916fa363ab997488babaf11f3b036a91
Niklas Hofer <niklas+dev@lanpartei.de>
2014-02-13 14:20:12 +0100

    sprite-position ignoring the group

--- lib/sprite_factory/sass_extensions.rb
+++ lib/sprite_factory/sass_extensions.rb
@@ -1,10 +1,51 @@
 require 'sass'
 module SpriteFactory
+  # provides Sass functions to access attributes of sprites.
+  #
+  # Strongly inspired by railsjedi/sprite
   module SassExtensions
-    def sprite_position(group, name)
-      xoff = '0px'
-      yoff = '65px'
+    def sprite_position(group, image, x=nil, y=nil)
+      xoff = compute_offset(group, image, :x, x)
+      yoff = compute_offset(group, image, :y, y)
       ::Sass::Script::String.new "#{xoff} #{yoff}"
+    end
+
+  protected
+
+    def compute_offset(group, image, axis, offset)
+      if offset
+        val = offset.value
+        if val.is_a? Fixnum
+          (sprite_attr(axis, group, image).to_i + val).to_s + 'px'
+        else
+          val
+        end
+      else
+        sprite_attr(axis, group, image)
+      end
+    end
+
+    def sprite_attr(attr, group, image)
+      sprite = sprite_data(group, image)
+      if sprite
+        val = sprite.public_send(attr)
+        "#{val}px"
+      else
+        ""
+      end
+    end
+
+    def sprite_runner
+      @__sprite_runner ||= SpriteFactory::Runner.new('somewhere', nocss: true).tap do |runner|
+        runner.run!
+      end
+    end
+
+    # TODO consider group
+    def sprite_data(group, image)
+      sprite_runner.images.find do |generated|
+        generated.name_without_pseudo_class == image.value
+      end
     end
   end
 end
--- spec/sass_extensions_spec.rb
+++ spec/sass_extensions_spec.rb
@@ -8,11 +8,48 @@ describe SpriteFactory::SassExtensions do
 
   let(:obj) { Object.new.tap { |o| o.extend described_class } }
 
+  # Sass wraps its arguments
+  def sass_val(v)
+    double 'SassValue', value: v
+  end
+
+  RSpec::Matchers.define :be_sass do |string|
+    match do |actual|
+      actual.is_a?(::Sass::Script::String) && actual.to_s == string
+    end
+  end
+
   describe '#sprite_position' do
-    it 'generates sass snippet with relative position for the given sprite' do
-      sass = obj.sprite_position("common", "icon")
-      sass.should be_a(::Sass::Script::String)
-      sass.to_s.should == '0px 65px'
+    let(:icon) { double('Image(icon)'  , name_without_pseudo_class: 'icon') }
+    let(:logo) { double('Image(logo)'  , name_without_pseudo_class: 'logo') }
+    let(:images) {[
+      icon,
+      logo,
+      double('Image(unused)', name_without_pseudo_class: 'unused')
+    ]}
+    let(:runner) { double 'Runner', images: images }
+
+    def sprite_position(*a)
+      obj.sprite_position *(a.map { |e| sass_val(e) })
+    end
+
+    before :each do
+      obj.stub sprite_runner: runner
+    end
+    it 'returns sprite offset' do
+      icon.stub x: 0, y: 5
+      sprite_position("common", "icon").should be_sass('0px 5px')
+    end
+
+    it 'returns another sprite offset' do
+      logo.stub x: 0, y: 65
+      sprite_position("common", "logo").should be_sass('0px 65px')
+    end
+
+    it 'takes additional offset' do
+      logo.stub x: 0, y: 65
+      sprite_position("common", "logo", 3, 5).
+        should be_sass('3px 70px')
     end
   end
 end
